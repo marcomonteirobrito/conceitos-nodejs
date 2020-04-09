d8d1546c9339defcd7889b77636b12aa9a8643a8
Marco Antonio Monteiro de Brito <marco_monteiro@live.com>
2020-04-09 20:32:44 -0300

    Editado método DELETE

--- src/app.js
+++ src/app.js
@@ -50,13 +50,12 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  const repository = repositories.find(repository => repository.id == id);
+  const findRepository = repositories.findIndex(repository => repository.id == id);
 
-  if (!repository) {
-    return response.status(400).send();
-  }
+  if (findRepository >= 0) {
+    repositories.splice(findRepository, 1);
+  } else response.status(400).json({ eror: 'Repository does not exists.'});
 
-  repositories.splice(repository, 1)
   return response.status(204).send();
   
 });
